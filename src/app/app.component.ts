3ce34f993a82dbecdc3cab2eef5c27a92464ac30
Scott Schretzenmaier <schretzilla@gmail.com>
2018-01-14 20:14:32 -0800

    Parallax fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Directive } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css', '../style/bootstrap-3.3.6/css/bootstrap.min.css', '../style/home.css']
+  styleUrls: [
+    './app.component.css', 
+    '../style/bootstrap-3.3.6/css/bootstrap.min.css', 
+    '../style/home.css'],
 })
 export class AppComponent {
   title = 'app works!';
